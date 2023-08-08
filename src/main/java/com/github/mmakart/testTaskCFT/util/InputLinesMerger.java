250fc5ab12a69b4fac64e646eb50e898eeca6eda
Mikhail Makartsev <m.p.makartsev@gmail.com>
2023-08-08 22:47:26 +0600

    Refactor: move copypaste code to method

--- src/main/java/com/github/mmakart/testTaskCFT/util/InputLinesMerger.java
+++ src/main/java/com/github/mmakart/testTaskCFT/util/InputLinesMerger.java
@@ -47,17 +47,8 @@ public class InputLinesMerger implements Iterable<String> {
                 String minOrMax = lastLines.stream().min(comparator).get();
 
                 int minOrMaxIndex = lastLines.indexOf(minOrMax);
-                String lastLine = null;
-                do {
-                    try {
-                        lastLine = readers.get(minOrMaxIndex).readLine();
-                    } catch (IOException e) {
-                        System.err.println("Warning: error occurred while reading an input file");
-                    }
-                } while (lastLine != null && !isLineValid.test(lastLine));
 
-                lastLines.set(minOrMaxIndex, lastLine);
-                listEOF.set(minOrMaxIndex, lastLine == null);
+                readNextValidLine(minOrMaxIndex);
 
                 return minOrMax;
             }
@@ -68,18 +59,22 @@ public class InputLinesMerger implements Iterable<String> {
 
             private void firstIteration() {
                 for (int i = 0; i < readers.size(); i++) {
-                    String line = null;
-                    do {
-                        try {
-                            line = readers.get(i).readLine();
-                        } catch (IOException e) {
-                            System.err.println("Warning: error occurred while reading an input file");
-                        }
-                    } while (line != null && !isLineValid.test(line));
-
-                    lastLines.set(i, line);
-                    listEOF.set(i, line == null);
+                    readNextValidLine(i);
                 }
+            }
+
+            private void readNextValidLine(int index) {
+                String line = null;
+                do {
+                    try {
+                        line = readers.get(index).readLine();
+                    } catch (IOException e) {
+                        System.err.println("Warning: error occurred while reading an input file");
+                    }
+                } while (line != null && !isLineValid.test(line));
+
+                lastLines.set(index, line);
+                listEOF.set(index, line == null);
             }
 
         };
